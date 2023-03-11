27de85694ae2a1beadbd91cc506702fe9c0dd934
micielski <r.micielski@protonmail.com>
2023-03-11 23:43:30 +0100

    Add a test, some API, some structs

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,35 @@
+use json::WallhavenSettings;
+use reqwest::Client;
+
+mod json;
+
 struct WallhavenUser {
     api_key: String,
+    client: Client,
 }
 
 impl WallhavenUser {
-    fn new(api_key: String) -> Self {
-        WallhavenUser { api_key }
+    pub fn new(api_key: String) -> Result<Self, Box<dyn std::error::Error>> {
+        let client = Self::create_client(api_key.clone());
+        // TODO: check api key for correctness
+        Ok(WallhavenUser { api_key, client })
+    }
+
+    pub fn settings(&self) -> WallhavenSettings {
+        todo!()
+    }
+
+    fn create_client(api_key: String) -> Client {
+        todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn creating_wallhaven_user() {
+        let _user = WallhavenUser::new("erwiopjweoir".to_owned());
     }
 }
